cbaedb8550794d7320a357261d50cd8052b27956
Bucha1958 <okoriemmadu@gmail.com>
2023-04-25 10:58:30 +0000

    File created with puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# Creates a file with puppet
+node default {
+file { '/tmp/school':
+ensure  => 'present',
+mode    => '0744',
+group   => 'www-data',
+owner   => 'www-data',
+content => 'I love Puppet',
+}
+}
