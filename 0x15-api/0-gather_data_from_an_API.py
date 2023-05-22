38bba905a1952f634119d0d10429435105c14111
Bucha1958 <okoriemmadu@gmail.com>
2023-05-22 22:23:03 +0000

    corrected file

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -19,18 +19,18 @@ def get_todo_progress(employee_id):
         user_response = requests.get(user_url)
         if user_response.status_code == 200:
             get_json = user_response.json()
-            employee_name = get_json.get("name")
-        number_tasks = len(todo_list)
-        task_completed = 0
-        task_list = ""
+            EMPLOYEE_NAME = get_json.get("name")
+        TOTAL_NUMBER_OF_TASKS = len(todo_list)
+        NUMBER_OF_DONE_TASKS = 0
+        TASK_TITLE = ""
         
         for task in todo_list:
             if task.get("completed") is True:
-                task_completed += 1
-                task_list += "\t " + task.get("title") + "\n"
+                NUMBER_OF_DONE_TASKS += 1
+                TASK_TITLE += "\t " + task.get("title") + "\n"
         
-        print("Employee {} is done with tasks({}/{})".format(employee_name,
-            task_completed, number_tasks))
+        print("Employee {} is done with tasks({}/{})".format(EMPLOYEE_NAME,
+            NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
         print(task_list[:-1])
 
 
