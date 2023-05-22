a61e4e8d8504f1513e72f78696af7a2602b8558b
Bucha1958 <okoriemmadu@gmail.com>
2023-05-22 23:03:11 +0000

    corrected file

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """
-    A python script that uses REST API that returns 
-    information about an employees' 
-    TODO list based on his/her ID information
+    A python script that uses REST API
 """
 import requests
 from sys import argv
@@ -12,26 +10,23 @@ def get_todo_progress(employee_id):
     rest_api_url = "https://jsonplaceholder.typicode.com"
     url = "{}/todos?userId={}".format(rest_api_url, employee_id)
     response = requests.get(url)
+    todo_list = response.json()
+    number_tasks = len(todo_list)
 
-    if response.status_code == 200:
-        todo_list = response.json()
-        user_url = "{}/users/{}".format(rest_api_url, employee_id)
-        user_response = requests.get(user_url)
-        if user_response.status_code == 200:
-            get_json = user_response.json()
-            EMPLOYEE_NAME = get_json.get("name")
-        TOTAL_NUMBER_OF_TASKS = len(todo_list)
-        NUMBER_OF_DONE_TASKS = 0
-        TASK_TITLE = ""
-        
-        for task in todo_list:
-            if task.get("completed") is True:
-                NUMBER_OF_DONE_TASKS += 1
-                TASK_TITLE += "\t " + task.get("title") + "\n"
-        
-        print("Employee {} is done with tasks({}/{})".format(EMPLOYEE_NAME,
-            NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
-        print(task_list[:-1])
+    user_url = "{}/users/{}".format(rest_api_url, employee_id)
+    user_response = requests.get(user_url)
+    get_json = user_response.json()
+    employee_name = get_json.get("name")
+
+    number_of_done_tasks = 0
+    task_title = ""
+    for task in todo_list:
+        if task.get("completed") is True:
+            number_of_done_tasks += 1
+            task_title += "\t " + task.get("title") + "\n"
+    print(f"Employee {employee_name} is done with tasks 
+            ({number_of_done_tasks}/{number_tasks})")
+    print(task_title[:-1])
 
 
 if __name__ == "__main__":
